86b5b00a55967ee35021660429a44404100720a0
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2024-02-06 15:36:47 +0100

    kapitel03

--- kapitel03/webapp/controller/App.controller.js
+++ kapitel03/webapp/controller/App.controller.js
@@ -9,6 +9,21 @@ sap.ui.define([
             onInit: function () {
                 var oData = models.createDataModel();
                 this.getView().setModel(oData, 'person');
+            },
+
+            onPress: function(oEvent){
+                
+                
+            },
+
+            onSelectionChange: function(oEvent){
+
+                var oSelectedItem = oEvent.getParameter("listItem");
+                var oBindingContext = oSelectedItem.getBindingContext("person");
+                var sPath = oBindingContext.getPath()
+                var oForm = this.byId("addressForm")
+                var sBinding = "person>" + sPath + "/Adresse"
+                oForm.bindElement(sBinding)
             }
 
         });
