b7ee6f57b853dd3197db28d74c4d238fc7dcb720
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2024-02-07 16:39:27 +0100

    kapitel 3.6

--- kapitel03/webapp/controller/App.controller.js
+++ kapitel03/webapp/controller/App.controller.js
@@ -1,21 +1,52 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'de/sapui5buch/demo/model/models'
-], function (Controller, models) {
+    'de/sapui5buch/demo/controller/BaseController',
+    'de/sapui5buch/demo/model/models',
+    'sap/m/MessageBox',
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+  
+], function (BaseController, models, MessageBox, Filter, FilterOperator) {
     'use strict';
-    return Controller.extend('de.sapui5buch.demo.controller.App',
+    return BaseController.extend('de.sapui5buch.demo.controller.App',
         {
 
             onInit: function () {
                 var oData = models.createDataModel();
-                this.getView().setModel(oData, 'person');
+                this.setModel(oData, 'person');
             },
 
             onPress: function(oEvent){
                 
                 
             },
+            onHello: function(oEvent){
 
+                var sValue = this.byId("idInputField").getValue()
+
+                MessageBox.show(
+                    "Hallo " + sValue, {
+                      icon: MessageBox.Icon.INFORMATION,
+                      title: "Hallo Nachricht",
+                      }
+                  );
+                
+            },
+            onSuggest: function(oEvent){
+
+                var sValue = oEvent.getParameter("suggestValue")
+                var aFilters = []
+                if(sValue){
+                    aFilters.push(new Filter("BusinessPartnerID", FilterOperator.StartsWith, sValue))
+                }
+
+                var oSource = oEvent.getSource()
+                var oBinding = oSource.getBinding("suggestionItems")
+                //oEvent.getSource().getBinding("suggestionItems").filter(aFilters)
+                console.log(oBinding)
+            },
+            onSuggestionItemSelected: function(oEvent){
+                console.log(oEvent)
+            },
             onSelectionChange: function(oEvent){
 
                 var oSelectedItem = oEvent.getParameter("listItem");
