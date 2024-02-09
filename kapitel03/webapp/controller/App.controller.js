d90d8f8a83d013d20be5e39cd037c1b0d0df8aac
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2024-02-09 15:17:37 +0100

    kapitel 6

--- kapitel03/webapp/controller/App.controller.js
+++ kapitel03/webapp/controller/App.controller.js
@@ -1,32 +1,33 @@
 sap.ui.define([
     'de/sapui5buch/demo/controller/BaseController',
     'de/sapui5buch/demo/model/models',
-    'sap/m/MessageBox'
-], function (BaseController, models, MessageBox) {
+    'sap/m/MessageBox',
+    'sap/ui/core/Fragment'
+], function (BaseController, models, MessageBox, Fragment) {
     'use strict';
     return BaseController.extend('de.sapui5buch.demo.controller.App',
         {
 
-            onInit: function(){
+            onInit: function () {
                 var oData = models.createDataModel();
                 this.setModel(oData, 'person');
-    
+
             },
-            onHello: function(oEvent){
+            onHello: function (oEvent) {
 
                 var sFirstname = this.byId("firstName").getValue()
                 var sLastname = this.byId("lastName").getValue()
-                var sValue = `${sFirstname} ${sLastname}` 
+                var sValue = `${sFirstname} ${sLastname}`
 
                 MessageBox.show(
                     "Hallo " + sValue, {
-                      icon: MessageBox.Icon.INFORMATION,
-                      title: "Hallo Nachricht",
-                      }
-                  );
-                
+                    icon: MessageBox.Icon.INFORMATION,
+                    title: "Hallo Nachricht",
+                }
+                );
+
             },
-            onSelectionChange: function(oEvent){
+            onSelectionChange: function (oEvent) {
 
                 var oSelectedItem = oEvent.getParameter("listItem");
                 var oBindingContext = oSelectedItem.getBindingContext("person");
@@ -34,6 +35,28 @@ sap.ui.define([
                 var oForm = this.byId("addressForm")
                 var sBinding = "person>" + sPath + "/Adresse"
                 oForm.bindElement(sBinding)
+            },
+            onOpenDialog: function (oEvent) {
+
+                if (!this.byId("myDialog")) {
+                    Fragment.load({
+                        id: this.getView().getId(),
+                        name: "de.sapui5buch.demo.view.PersonDialog",
+                        type: "XML",
+                        controller: this
+                    }).then(function (oDialog) {
+                        oView = this.getView();
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    }).catch(function (error) {
+                        console.log(error.message)
+                    });
+                } else {
+                    this.byId("myDialog").open();
+                }
+            },
+            onCloseDialog: function (oEvent) {
+                this.byId("myDialog").close();
             }
 
         });
--- kapitel03/webapp/controller/Personal.controller.js
+++ kapitel03/webapp/controller/Personal.controller.js
@@ -22,14 +22,53 @@ sap.ui.define([
                     aFilters.push(new Filter("BusinessPartnerID", FilterOperator.StartsWith, sValue))
                 }
 
-                var oSource = oEvent.getSource()
-                var oBinding = oSource.getBinding("suggestionItems")
-                oBinding.filter(aFilters)
+                oEvent.getSource().getBinding("suggestionItems").filter(aFilters);
                 
             },
             onSuggestionItemSelected: function(oEvent){
-                console.log(oEvent)
+
+                var oSelectedItem = oEvent.getParameter("selectedItem")
+                var sBindingPath = oSelectedItem.getBindingContext("personal").getPath()
+                this._updateUI("personal>" + sBindingPath)
             },
+            _updateUI: function(sBindingPath){
+
+                var oHeaderContent = this.byId("headerContent")
+                oHeaderContent.bindElement(sBindingPath)
+
+                var oObjLayout = this.byId("ObjectPageLayout");
+                oObjLayout.setShowHeaderContent(true);
+                oObjLayout.setToggleHeaderOnTitleClick(true);
+            },
+            onPlus: function(oEvent){
+                
+                var oCnt = this.byId("cntLbl")
+                var value = Number(oCnt.getProperty("text"))
+                value++
+                oCnt.setProperty("text", value)
+            },
+            onMinus: function(oEvent){
+
+                var oCnt = this.byId("cntLbl")
+                var value = Number(oCnt.getProperty("text"))
+                value--
+                oCnt.setProperty("text", value)
+            },
+            onValueHelpRequest : function(oEvent) {
+                var sInputValue = oEvent.getSource().getValue();
+                this._sInputId = oEvent.getSource().getId();
+                if (!this._oValueHelpDialog) {
+                 this._oValueHelpDialog = sap.ui.xmlfragment(
+                    "de.sapui5buch.demo.view.PersonDialog",
+                    this);
+                 this.getView().addDependent(this._oValueHelpDialog);
+                }
+                this._oValueHelpDialog.getBinding("items").filter(
+                  [new Filter("BusinessPartnerID",
+                   FilterOperator.Contains, sInputValue
+                )]);
+                this._oValueHelpDialog.open(sInputValue);
+             }
 
         });
 });
