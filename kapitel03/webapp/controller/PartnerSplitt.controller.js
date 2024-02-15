624a64fd21d6215e0d89bd94cbefc46d9750f481
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2024-02-15 16:06:04 +0100

    group

--- kapitel03/webapp/controller/PartnerSplitt.controller.js
+++ kapitel03/webapp/controller/PartnerSplitt.controller.js
@@ -1,12 +1,22 @@
 sap.ui.define([
     'de/sapui5buch/demo/controller/BaseController',
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-
-], function (BaseController, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/ui/core/Fragment"
+], function (BaseController, Filter, FilterOperator, Sorter, Fragment) {
     'use strict';
+
     return BaseController.extend('de.sapui5buch.demo.controller.PartnerSplitt',
         {
+
+            onInit: function () {
+                this.bGrouped = true
+                this.bDescending = false
+                this.sSearchQuery = 0
+                this._mViewSettingsDialogs = {}
+                this._oFilterDialog = null
+            },
 
             onSuggest: function (oEvent) {
 
@@ -34,11 +44,11 @@ sap.ui.define([
                 oContent.setProperty("visible", true)
 
                 var oHeaderContent = this.byId("headerContent")
-               // oHeaderContent.bindElement(sBindingPath)
+                // oHeaderContent.bindElement(sBindingPath)
                 oHeaderContent.setProperty("visible", true)
 
                 var oAddressContent = this.byId("addressContent")
-               // oAddressContent.bindElement(sBindingPath)
+                // oAddressContent.bindElement(sBindingPath)
                 oAddressContent.setProperty("visible", true)
 
                 var oOrdersTable = this.byId("ordersTable")
@@ -95,7 +105,84 @@ sap.ui.define([
 
                 //console.log(oEvent)
                 var oPull = this.byId("pullToRefreshPartners").hide()
+            },
+            onSortPartnerTable: function (oEvent) {
+
+                this.bDescending = !this.bDescending;
+                this.fnApplyFiltersAndOrdering();
+
+            },
+            onGroupPress: function (oEvent) {
+
+                this._loadFragment(
+                    "de.sapui5buch.demo.view.GroupSettingsDialog").then(
+                        function (oDialog) {
+                            oDialog.open();
+                        });
+            },
+            fnApplyFiltersAndOrdering: function (oEvent) {
+                var aFilters = [],
+                    aSorters = [];
+
+
+                if (this.bGrouped) {
+                    aSorters.push(new Sorter("LegalForm", this.bDescending, this._fnGroup));
+                } else {
+                    aSorters.push(new Sorter("LegalForm", this.bDescending));
+                }
+
+                if (this.sSearchQuery) {
+                    var oFilter = new Filter("CompanyName", FilterOperator.Contains, this.sSearchQuery);
+                    aFilters.push(oFilter);
+                }
+
+                this.byId("businessPartnerTable").getBinding("items").filter(aFilters).sort(aSorters);
+            },
+            _fnGroup: function (oContext) {
+                var sLegalForm = oContext.getProperty("LegalForm");
+
+                return {
+                    key: sLegalForm,
+                    text: sLegalForm
+                };
+            },
+            _loadFragment: function (sFragmentName) {
+
+                return new Promise(function (resolve) {
+                    var oDialog = this._mViewSettingsDialogs[sFragmentName];
+                    if (!oDialog) {
+                        Fragment.load({
+                            name: sFragmentName,
+                            type: "XML",
+                            controller: this
+                        }).then(function (oDialog) {
+                            this._oFilterDialog = oDialog;
+                            this.getView().addDependent(oDialog);
+                            this._mViewSettingsDialogs[sFragmentName] = oDialog;
+                            resolve(oDialog);
+                        }.bind(this));
+                    } else {
+                        resolve(oDialog);
+                    }
+                }.bind(this));
+
+            },
+            onGroupDialogConfirm: function (oEvent) {
+                var mParams = oEvent.getParameters()
+                var oBinding = this.byId("businessPartnerTable").getBinding("items")
+                var sPath = ''
+                var aGroups = []
+                var bDescending = false
+
+                if(mParams.groupItem){
+                    sPath = mParams.groupItem.getKey();
+                    bDescending = mParams.groupDescending;
+                    aGroups.push(new Sorter(sPath, bDescending, true));        
+                    oBinding.sort(aGroups);
+                }
+              
             }
+
 
 
         });
